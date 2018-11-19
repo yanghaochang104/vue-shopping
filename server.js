b781987b1aeb4a3d4c8ce980513cfa98d344c7a9
YangHao Chang <yanghaochang1004@gmail.com>
2018-11-19 16:47:24 +0000

    write in express

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 var express = require('express');
-var serveStatic = require('serve-static');
 var app = express();
 
-app.use(serveStatic(__dirname + "/dist"));
-
+app.use(express.static(__dirname + "/dist"));
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("server started");
